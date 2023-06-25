998baad7a98624242835bf6569c932e37dfc86f1
crisidoly <cristhiansidoly@gmail>
2023-06-25 20:31:26 -0300

    Finished

--- src/app.js
+++ src/app.js
@@ -3,17 +3,15 @@ import cors from "cors";
 
 const app = express();
 const PORT = 5000;
+const users = [];
+const tweets = [];
 
 app.use(cors());
 app.use(express.json());
 
-const users = [];
-const tweets = [];
-
 app.post("/sign-up", (req, res) => {
   const { username, avatar } = req.body;
   users.push({ username, avatar });
-  console.log(users);
   res.send("OK");
 });
 
@@ -31,8 +29,8 @@ app.post("/tweets", (req, res) => {
 });
 
 app.get("/tweets", (req, res) => {
-  const lastTenTweets = tweets.slice(-10).map((tweet) => {
-    const user = users.find((u) => u.username === tweet.username);
+  const lastTenTweets = tweets.slice(-10).map(tweet => {
+    const user = users.find(u => u.username === tweet.username);
     return { ...tweet, avatar: user.avatar };
   });
   res.send(lastTenTweets);

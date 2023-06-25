d0c6ab3df3e78e0db9f08f9e3e1f5d950adb1f00
crisidoly <cristhiansidoly@gmail>
2023-06-25 19:55:07 -0300

    posts e get prontos

---
+++ src/app.js
@@ -0,0 +1,43 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+const PORT = 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const users = [];
+const tweets = [];
+
+app.post("/sign-up", (req, res) => {
+  const { username, avatar } = req.body;
+  users.push({ username, avatar });
+  console.log(users);
+  res.send("OK");
+});
+
+app.post("/tweets", (req, res) => {
+  const {username, tweet} = req.body;
+
+  const realUser = users.find(user => user.username === username);
+  
+  if (!realUser) { return res.send("UNAUTHORIZED"); }
+
+  tweets.push({username, tweet})
+  res.send("OK")
+})
+
+app.get("/tweets", (req, res) => {
+  const tweetsDone = tweets.map((tweet) => {
+    const user = users.find((u) => u.username === tweet.username)
+    return{...tweet, avatar: user.avatar}
+
+  })
+  res.send(tweetsDone.slice(-10))
+
+})
+
+app.listen(PORT, () => {
+  console.log(`Rodando na porta ${PORT}`);
+});
